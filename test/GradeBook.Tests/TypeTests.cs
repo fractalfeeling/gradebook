8008b1730e420f7003d640fec1bffced6e66550c
Shyen Wee <swee@tictoc.com.au>
2023-01-06 16:36:10 +1030

    upload files

---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,92 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests;
+
+public class TypeTests
+{
+    [Fact]
+    public void ValueTypesAlsoPassByValue()
+    {
+        var x = GetInt();
+        SetInt(ref x);
+        Assert.Equal(42, x);
+    }
+
+    private int GetInt()
+    {
+        return 3;
+    }
+
+    private void SetInt(ref int z)
+    {
+        z = 42;
+    }
+
+    [Fact]
+    public void CSharpIsPassByRef()
+    {
+        var book1 = GetBook("Book 1");
+        GetBookSetName(ref book1, "New Name");
+
+        Assert.Equal("New Name", book1.Name);
+    }
+
+    private void GetBookSetName(ref Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+    [Fact]
+    public void CanSetNameFromReference()
+    {
+        var book1 = GetBook("Book 1");
+        SetName(book1, "New Name");
+
+        Assert.Equal("New Name", book1.Name);
+    }
+
+    private void SetName(Book book, string name)
+    {
+        book.Name = name;
+    }
+
+    [Fact]
+    public void GetBookReturnsDifferentObjects()
+    {
+        var book1 = GetBook("Book 1");
+        var book2 = GetBook("Book 2");
+
+        Assert.Equal("Book 1", book1.Name);
+        Assert.Equal("Book 2", book2.Name);
+        Assert.NotSame(book1, book2);       
+    }
+
+    [Fact]
+    public void TwoVarsCanReferenceSameObject()
+    {
+        var book1 = GetBook("Book 1");
+        var book2 = book1;
+
+        Assert.Same(book1, book2);       
+    }
+
+    [Fact]
+    public void StringsBehaveLikeValueTypes()
+    {
+        string name = "Shyen";
+        var upper = MakeUpperCase(name);
+
+        Assert.Equal("SHYEN", upper);
+    }
+
+    private string MakeUpperCase(string parameter)
+    {
+        return parameter.ToUpper();
+    }
+
+    Book GetBook(string name)
+    {
+        return new Book(name);
+    }
+}
